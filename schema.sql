871ee4ac87e17edec319f7f21ece8be918c807db
Kyle Shepherd <kylejamesshepherd@gmail.com>
2024-07-31 07:53:50 -0500

    addition of nfl schedule to schema

--- schema.sql
+++ schema.sql
@@ -1,36 +1,3 @@
--- CREATE TABLE Teams (
---     id SERIAL PRIMARY KEY,
---     name VARCHAR(255),
---     abbreviation VARCHAR(10),
---     city VARCHAR(255)
--- );
-
--- CREATE TABLE Players (
---     id SERIAL PRIMARY KEY,
---     name VARCHAR(255),
---     team_id INT REFERENCES Teams(id),
---     position VARCHAR(50)
---     ranking INT,
---     year INT
--- );
-
--- CREATE TABLE Games (
---     id SERIAL PRIMARY KEY,
---     date DATE,
---     home_team_id INT REFERENCES Teams(id),
---     away_team_id INT REFERENCES Teams(id),
---     home_team_score INT,
---     away_team_score INT
--- );
-
--- CREATE TABLE Stats (
---     id SERIAL PRIMARY KEY,
---     player_id INT REFERENCES Players(id),
---     game_id INT REFERENCES Games(id),
---     stat_name VARCHAR(255),
---     stat_value FLOAT
--- );
-
 CREATE TABLE Team_statistics (
     year INT,
     team VARCHAR(100),
@@ -41,5 +8,14 @@ CREATE TABLE Team_statistics (
     dpr DOUBLE PRECISION
 );
 
-\COPY Team_statistics(year, team, srs, osrs, dsrs, qbr, dpr) FROM '/sql/data/fullTeamData.csv' DELIMITER ',' CSV HEADER;
+\COPY Team_statistics FROM '/Users/kyleshepherd/Downloads/ParlayPerfector/sql/data/fullTeamData.csv' DELIMITER ',' CSV HEADER;
 
+create table nfl_schedule( 
+    year INT, 
+    week INT,
+    home_team VARCHAR(100),
+    away_team VARCHAR(100),
+    home_points INT,
+    margin INT,
+    total INT
+);
